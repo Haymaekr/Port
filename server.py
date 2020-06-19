f7530631720c681e9fca88a5b11e018ad2e250fe
Jack Dawn <abhayharidas@gmail.com>
2020-06-19 21:21:29 +0530

    My portfolio

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, send_from_directory, request,redirect
+import csv
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def first_page():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_csv(data):
+	with open('database.csv', mode='a',newline="") as csvfile:
+		email = data['email']
+		subject = data['subject']
+		message = data['message']
+		csv_writer = csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method=="POST":
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return redirect('thankyou.html')
+
+		except:
+			return "Database not saved"
+	else:
+		return "Try again"
